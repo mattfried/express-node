c2714decef8d8454b0be4fdb16fa968111c4ebd6
Matt Fried <mattfried@unknown101f748dc1c5.gateway.2wire.net>
2019-05-17 22:23:15 -0500

    added card.pug with logical programming

--- app.js
+++ app.js
@@ -2,15 +2,21 @@ const express = require('express');
 
 const app = express();
 
+app.set('view engine', 'pug');
+
 app.get('/', (req, res) => {
-  res.send('<h1>I love skateboarding</h1>');
+  res.render('index');
 });
 
-app.get('/hello', (req, res) => {
-  res.send('<h1>Sup! coding coding coding!</h1>');
+app.get('/cards', (req, res) => {
+  res.render('card', { prompt: "Who won the hockey game tonight?", hint: "It wasn't San Jose Sharks."});
 });
 
 
 app.listen(3000, () => {
   console.log('The application is running on locahost:3000!')
 });
+
+// once 'nodemon' is installed
+// run 'nodemon' command instaed of having to
+// manually stop and restart server
